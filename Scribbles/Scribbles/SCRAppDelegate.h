f6346dc2b3c36e8df3f04b17138b28ed3c349b32
jhend11 <jhend11@uw.edu>
2014-08-04 09:17:17 -0400

    Started Scribbles

---
+++ Scribbles/Scribbles/SCRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCRAppDelegate.h
+//  Scribbles
+//
+//  Created by JOSH HENDERSHOT on 8/4/14.
+//  Copyright (c) 2014 Joshua Hendershot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
